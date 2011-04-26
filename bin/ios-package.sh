c6f1133d99b67eb32c9352c5e949e3b5ca61383f
David Wagner <mnem@noiseandheat.com>
2011-04-26 10:28:38 +0100

    Corrected descriptor names

--- bin/ios-package.sh
+++ bin/ios-package.sh
@@ -4,8 +4,8 @@ SIGNING_ROOT=~/Mobile_Flash
 KEYSTORE=$SIGNING_ROOT/dww-iphone.p12
 PROVISIONING=$SIGNING_ROOT/Flash_Profile.mobileprovision
 HOST_IP=127.0.0.1
-DESCRIPTOR=PlayingPunkApp-app.xml
-OUTPUT_NAME=PlayingPunkApp.ipa
+DESCRIPTOR=PlayingFlashApp-app.xml
+OUTPUT_NAME=PlayingFlashApp.ipa
 
 INPUT_FILES="\
 PlayingFlashApp.swf \
